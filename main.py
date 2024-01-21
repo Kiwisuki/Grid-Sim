fa929ce0e16075186d405efc0c07ebf274fdf213
Marius <marius.arlauskas01.dev@gmail.com>
2024-01-22 01:33:41 +0200

    minor tweaks

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+import logging
+
 import gif
 import pandas as pd
+from alive_progress import alive_bar
 
 from config.constants import (
     COORDS,
@@ -12,8 +16,10 @@ from config.constants import (
 from src.animation import plot_comparison
 from src.simulation import Population
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
-def run_simulation():
+
+def run_simulation(gen_gif: bool = False):
     pop_a = Population(GRID_SIZE, INITIAL_INFECTED, coords=COORDS)
     pop_b = Population(GRID_SIZE, INITIAL_INFECTED, coords=COORDS)
     pop_b.isolate_subpopulations()
@@ -35,29 +41,48 @@ def run_simulation():
         + pop_b.count_states()['Recovered']
     )
 
-    frames = []
+    if gen_gif:
+        frames = []
 
-    for _ in range(SIM_DAYS):
-        print('Day ' + str(_ + 1), end='\r')
+    with alive_bar(SIM_DAYS) as bar:
+        for _ in range(SIM_DAYS):
+            bar()
 
-        pop_a.update(INFECTION_RATE, RECOVERY_RATE, 0)
-        grids_a.append(pop_a.get_matrix())
-        ts_a.append(pop_a.count_states())
+            pop_a.update(INFECTION_RATE, RECOVERY_RATE, 0)
+            grids_a.append(pop_a.get_matrix())
+            ts_a.append(pop_a.count_states())
 
-        pop_b.update(INFECTION_RATE, RECOVERY_RATE, 0)
-        grids_b.append(pop_b.get_matrix())
-        ts_b.append(pop_b.count_states())
+            pop_b.update(INFECTION_RATE, RECOVERY_RATE, 0)
+            grids_b.append(pop_b.get_matrix())
+            ts_b.append(pop_b.count_states())
 
-        ts_a_plot = pd.DataFrame(ts_a)['Infected'] / size_a * 100
-        ts_b_plot = pd.DataFrame(ts_b)['Infected'] / size_b * 100
+            if gen_gif:
+                ts_a_plot = pd.DataFrame(ts_a)['Infected'] / size_a * 100
+                ts_b_plot = pd.DataFrame(ts_b)['Infected'] / size_b * 100
 
-        grid_a = grids_a[-1]
-        grid_b = grids_b[-1]
+                grid_a = grids_a[-1]
+                grid_b = grids_b[-1]
 
-        frames.append(plot_comparison(grid_a, ts_a_plot, grid_b, ts_b_plot))
+                frames.append(plot_comparison(grid_a, ts_a_plot, grid_b, ts_b_plot))
 
-    gif.save(frames, 'output/comparison.gif', duration=100)
+    if gen_gif:
+        logging.info('Generating GIF...')
+        gif.save(frames, 'output/comparison.gif', duration=100)
+
+    return grids_a, ts_a, grids_b, ts_b
 
 
 if __name__ == '__main__':
-    run_simulation()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-g',
+        '--gif',
+        help='Generate a GIF of the simulation.',
+        action='store_true',
+    )
+    args = parser.parse_args()
+    logging.info('Running simulation...')
+    logging.info('Generating GIF: ' + str(args.gif))
+
+    run_simulation(args.gif)
+    logging.info('Simulation complete.')
